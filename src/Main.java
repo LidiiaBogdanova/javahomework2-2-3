25858c770646fa8919ae871424c913f1d05af793
LidiiaBogdanova <novikova71095@gmail.com>
2021-03-16 17:42:27 +0300

    CreditPaymentService edited;code reformated

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        CreditPaymentService service= new CreditPaymentService();
-
-
-
-       int payment = service.calculate(1_000_000,1);
-       System.out.println(payment);
-       payment = service.calculate(1_000_000,2);
+        CreditPaymentService service = new CreditPaymentService();
+        int payment = service.calculate(1_000_000, 1);
         System.out.println(payment);
-        payment = service.calculate(1_000_000,3);
+        payment = service.calculate(1_000_000, 2);
+        System.out.println(payment);
+        payment = service.calculate(1_000_000, 3);
         System.out.println(payment);
     }
 }
